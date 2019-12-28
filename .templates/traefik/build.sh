461d2f550b81d193bb0fb71e9435eb52511dc469
hepelayo <hepelayo@gmail.com>
2019-12-28 15:57:33 +0100

    Updated Traefik builder

--- .templates/traefik/build.sh
+++ .templates/traefik/build.sh
@@ -4,9 +4,10 @@
 
 function configTraefik(){
     # Create the file where the certs will be saved if not exists
-    [ -d ./certs ] && mkdir ./certs
+    [ ! -d ./certs ] && mkdir ./certs
     [ -f ./certs/acme.json ] && rm ./certs/acme.json
     touch ./certs/acme.json && chmod 600 ./certs/acme.json
+    echo -e "\e[93m""Created certificates folder""\e[0m"
 
     # Setup the traefik.env file
     clear
@@ -15,8 +16,7 @@ function configTraefik(){
     duckSubdomain=$(whiptail --title " DuckDNS account" --inputbox "Please enter your DuckDNS Subdomain:" 10 60 3>&1 1>&2 2>&3)
     whiptail --title "Traefik Dashboard Access" --msgbox "Next we will configure the access to the Traefik Dashboard" 10 60 3>&1 1>&2 2>&3
     traefikUsername=$(whiptail --title "Traefik Dashboard account" --inputbox "Please enter the desired username to access to the dashboard:" 10 60 3>&1 1>&2 2>&3)
-
-
+    
     if [ ! -z "$traefikUsername" ];then
         while [[ "$passphrase" != "$passphrase_repeat" || ${#passphrase} -lt 8 ]]; do
     		passphrase=$(whiptail --title "Traefik Dashboard account" --passwordbox "${passphrase_invalid_message}Please enter the passphrase (8 chars min.):" 10 60 3>&1 1>&2 2>&3)
@@ -27,22 +27,22 @@ function configTraefik(){
         traefikUserPass=$traefikUsername:`echo $passphrase | openssl passwd -apr1 -stdin`
         if [ ! -z "$traefikUserPass" ]; then
             if [[ $(grep -cs "TRAEFIK_HTTP_USERPASS=" .env) -gt 0 ]]; then
-                sed --in-place -re "s/^(TRAEFIK_HTTP_USERPASS=).*/\1$traefikUserPass/" .env
+                sed --in-place -r "s/^(TRAEFIK_HTTP_USERPASS=).*/\1$traefikUserPass/" .env
             else
                 echo "TRAEFIK_HTTP_USERPASS=$traefikUserPass" >> .env
             fi
-            sed --in-place -re "s/^#(      - traefik.frontend.auth.basic.users\.*)/\1/" ./docker-compose.yml
+            sed --in-place -r "s/^#(      - traefik.frontend.auth.basic.users\.*)/\1/" ./docker-compose.yml
         else
-            sed --in-place -re "s/^(      - traefik.frontend.auth.basic.users\.*)/#\1/" ./docker-compose.yml
+            sed --in-place -r "s/^(      - traefik.frontend.auth.basic.users\.*)/#\1/" ./docker-compose.yml
         fi
     else
         echo -e "Username not entered. This disable basic HTML authentication in Traefik Dashboard..."
-        sed --in-place -re "s/^(      - traefik.frontend.auth.basic.users\.*)/#\1/" ./docker-compose.yml
+        sed --in-place -r "s/^(      - traefik.frontend.auth.basic.users\.*)/#\1/" ./docker-compose.yml
     fi
-    
+    echo -e "\e[93m""Added basic authentication to Traefik Dashboard""\e[0m"
 
     if [ ! -z "$duckToken" -a ! -z "$duckSubdomain" ]; then
-        sed --in-place -re "s/^(DUCKDNS_TOKEN=).*/\1$duckToken/" ./services/traefik/traefik.env
+        sed --in-place -r "s/^(DUCKDNS_TOKEN=).*/\1$duckToken/" ./services/traefik/traefik.env
         
         # Setup crontab to update the Raspberry Pi IP in DuckDNS
         sed --in-place -re "s/^(DOMAINS=).*/\1$duckSubdomain/; s/^(DUCKDNS_TOKEN=).*/\1$duckToken/" ./duck/duck.sh
@@ -51,7 +51,7 @@ function configTraefik(){
 
         # Set .env file in docker-compose directory in order to easy the traefik's labels config
         if [[ $(grep -cs "DOMAIN_NAME=" .env) -gt 0 ]]; then
-            sed --in-place -re "s/^(DOMAIN_NAME=).*/\1$duckSubdomain.duckdns.org/" .env
+            sed --in-place -r "s/^(DOMAIN_NAME=).*/\1$duckSubdomain.duckdns.org/" .env
         else
             echo "DOMAIN_NAME=$duckSubdomain.duckdns.org" >> .env
         fi
